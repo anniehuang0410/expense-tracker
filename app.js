f6c300ed9fb566e971415109d3a9d4633e298a0f
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-24 14:37:32 +0800

    feat: add mongoose and expense & user models

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose') // 載入 mongoose
+
 const PORT = 3000
+
+// 僅在非正式環境使用 dotenv
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('MongoDB error.')
+} )
+db.once('open', () => {
+  console.log('MongoDB connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Initialization')
---
+++ models/expense.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const expenseSchema = new Schema ({
+  name: {
+    type: String, // 資料型別是字串
+    required: true
+  },
+  date: {
+    type: Date, // 資料型別是日期
+    required: true
+  },
+  amount: {
+    type: Number, // 資料型別是數字
+    required: true
+  },
+  userId: {
+
+  },
+  // v1.不能自行新增版本
+  categoryId: {
+    type: Number,
+    required: true
+  }
+})
+module.exports = mongoose.model('Expense', expenseSchema)
