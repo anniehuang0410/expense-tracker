876b91d340b755d2ef2eee47d8dc58f79cf575af
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-31 14:28:37 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars') // 載入 handlebars
 const methodOverride = require('method-override') // 載入 method-override
+const session = require('express-session') // 載入 cookie-session
 
 const routes = require('./routes') // 引入路由模組
 require('./config/mongoose')
@@ -15,6 +16,13 @@ const app = express()
 // set view engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// cookie-session
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
